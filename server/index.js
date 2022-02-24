ea4309390e6ddf312fdbbbed4dea4dbc84246df0
James Hager <james@avantiapplications.com>
2022-02-24 13:04:04 -0700

    Add axios

--- server/index.js
+++ server/index.js
@@ -29,11 +29,20 @@ app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('student was added successfully', {author: 'Scott', type: 'manual', student: name})
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
 
 app.use(rollbar.errorHandler())
